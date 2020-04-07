8b7c8959db0184b4ac5b55000fbdfc1589d0c4b4
Kelvin Murumba <kevmurumba@Kelvins-MacBook-Pro.local>
2020-04-07 15:58:18 +0300

    Working with Primitive Data Types

--- main.go
+++ main.go
@@ -2,6 +2,48 @@ package main
 
 import "fmt"
 
+//Constant blocks
+const (
+	first = iota + 6
+	second 
+)
 func main() {
 	fmt.Println("Hello from a module, Gophers!")
+
+	var lastName *string = new(string)
+	*lastName = "Murumba"
+	fmt.Println(*lastName)
+
+	/*
+	Working with Pointers
+	*/
+	firstName := "Kelvin"
+	fmt.Println(firstName)
+
+	ptr := &firstName
+	fmt.Println(ptr, *ptr)
+
+	firstName = "Trish"
+	fmt.Println(ptr, *ptr)
+
+	/*
+	Working with Constants
+	Constants are declared at the initial time
+	*/
+	const pi = 3.14
+	fmt.Println(pi)
+
+	const c = 3
+	fmt.Println(c + 3)
+
+	fmt.Println(c + 1.2)
+
+	/*
+	Working with iota
+	iota resets everytime it is in a new const block
+	*/
+	fmt.Println(first, second)
+
+
+
 }
