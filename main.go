d15c6c837d63b452606a7d4b64f7ae7c7b968d8b
Kelvin Murumba <kevmurumba@Kelvins-MacBook-Pro.local>
2020-04-09 22:19:25 +0300

    Working with arrays

--- main.go
+++ main.go
@@ -11,4 +11,14 @@ func main() {
 	arr := [3]int{1,2,3}
 	fmt.Println(arr)
 
+	/*
+	Working with Slices
+	It is not a fixed sized entity as an array
+	*/
+	slice := []int{1,2,3}
+	fmt.Println(slice)
+
+	slice = append(slice,4,42,27)
+	fmt.Println(slice)
+
 }
